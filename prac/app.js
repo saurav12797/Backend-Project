6ebbcc43445f2a054893c4ade1d3d2a3e9eb05f1
saurav <saurav12797@gmail.com>
2021-05-30 12:38:39 +0530

    1st database insertion

---
+++ prac/app.js
@@ -0,0 +1,45 @@
+
+const mongoose= require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/saurav",{useNewUrlParser:true,useUnifiedTopology: true } )
+.then(()=>console.log("Success"))
+.catch((err)=>console.log(err));
+
+
+
+const mymovieSchema=new mongoose.Schema({
+
+  movie:String,
+  actor:String,
+  budget:Number
+
+
+
+})
+
+// now to insert elements into table i.e create collections with model we insert
+
+const Mymovie= new mongoose.model("Mymovie",mymovieSchema) ; //creation of class
+
+//create document or insert in 1st { }
+
+
+
+ const reactMovie= new Mymovie({
+
+  movie:"aashiqi",
+  actor:"Saurav",
+  budget:5000
+
+
+
+ })
+
+ reactMovie.save();
+  
+ 
+
+
+
+
+
